14877f49d52ac0afc1eee1a0a433712133d2b7a8
Raka M.A <rakamuhammadalhafidz12312@gmail.com>
2022-02-18 09:45:19 +0700

    memberikan commenct

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,6 +1,39 @@
 import 'package:flutter/material.dart';
 
-//Fungsi main adalah fungsi yang akan dijalankan ketika pertamakali mulai project
+//Fungsi main adalah fungsi yang akan dijalankan ketika pertamakali mulai
+// project
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Container"),
+        ),
+        body: Container(
+          //margin: jarak antara container dengan widget lainnya
+          //(yang diluar container)
+
+          //padding: jarak dari sisi container ke widget yang ada didalam container
+          // widget pengisi: EdgeInsets
+          ///EdgeInsets.all() untuk margin semua sisi
+          ///EdgeInsets.fromLTRB() untuk margin, isi dari kiri, atas, kanan, bawah'
+          ///EdgeInsets.only() isi dengan left, right, top, atau bottom. seperti martgin_bottom, dst. di Android native
+          margin: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16),
+          color: Colors.red,
+          child: Container(
+            color: Colors.blue,
+          ),
+        ),
+      ),
+    );
+  }
 }
