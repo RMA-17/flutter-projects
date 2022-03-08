95c456ad416c25b1e834607338afad5516e9486d
Raka M.A <rakamuhammadalhafidz12312@gmail.com>
2022-03-08 14:06:24 +0700

    remove windows

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/pages/home_page.dart';
 
 //Fungsi main adalah fungsi yang akan dijalankan ketika pertamakali mulai
 // project
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
 }
---
+++ test_app/android/app/src/main/kotlin/com/rmaproject/test/flutter/test_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rmaproject.test.flutter.test_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
